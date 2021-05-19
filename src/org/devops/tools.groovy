2a59cd38fd0a1fc8376af05870b66ae406813c56
awesomewho <82640808+awesomewho@users.noreply.github.com>
2021-05-19 17:24:10 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -9,7 +9,7 @@ def PrintMes(content){
 def PrintMessage(value, color){
     colors = ['red' : "\033[40;31n *********** ${value}  *********** \033[0n",
                'blue' : "\033[47;34n *********** ${value} *********** \033[0n",
-               'green' : "[1;32m *********** ${value} *********** [n",
+               'green' : "\[1;32m *********** ${value} *********** [n",
                'green1' : "\033[40;32n *********** ${value} *********** \033[0n"
               ]
     ansiColor('xterm') {
