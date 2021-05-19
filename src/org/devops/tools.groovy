5ba8c5a766e1347a4c41ae4480cc76345becbc92
awesomewho <82640808+awesomewho@users.noreply.github.com>
2021-05-19 17:21:28 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -4,3 +4,15 @@ package org.devops
 def PrintMes(content){
   println(content)
 }
+
+// 安装ansiColor让日志输出颜色
+def PrintMessage(value, color){
+    colors = ['red' : "\033[40;31n *********** ${value}  *********** \033[0n",
+               'blue' : "\033[47;34n *********** ${value} *********** \033[0n",
+               'green' : "[1;32m *********** ${value} *********** [n",
+               'green1' : "\033[40;32n *********** ${value} *********** \033[0n"
+              ]
+    ansiColor('xterm') {
+        println(colors[color])
+    }
+}
