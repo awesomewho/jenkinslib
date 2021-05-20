2491eb0484d5d58e7c3195097c0f0b2d3f8f5cbd
awesomewho <82640808+awesomewho@users.noreply.github.com>
2021-05-20 14:16:10 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -7,5 +7,9 @@ def Build(buildType, buildShell) {
     println("当前选择的构建类型为  ${buildType}")
     buildHome = tool buildTools[buildType]
     
+    if ( "${buildType}" == "npm" ) {
+        sh "export NPM_HOME=${buildHome} && export PATH=\$NPM_HOME/bin:\$PATH && (${buildHome}/bin/${buildType} ${buildShell})"
+    } 
+    
     sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
