d9a897d0f404690d16a12a7290807a342505f156
awesomewho <82640808+awesomewho@users.noreply.github.com>
2021-05-20 12:29:20 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -7,5 +7,5 @@ def Build(buildType, buildShell) {
     println("当前选择的构建类型为  ${buildType}")
     buildHome = tool buildTools[buildType]
     
-    sh "${buildHome}/bin/${bulidType} ${buildShell}"
+    sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
