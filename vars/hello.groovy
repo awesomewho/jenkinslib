7cb3324ab6faae2e79da0b93a87fb918f38bdde7
awesomewho <82640808+awesomewho@users.noreply.github.com>
2021-05-24 10:53:11 +0800

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -5,9 +5,8 @@ def call(){
 // 安装 ansiColor 插件,打印日志
 def PrintMessage(value, color){
     colors = ['red' : "\033[40;31m *********** ${value}  *********** \033[0m\n",
-               'blue' : "\033[47;34n *********** ${value} *********** \033[0n",
-               'green' : "[1;32m *********** ${value} *********** [n",
-               'green1' : "\033[40;32n *********** ${value} *********** \033[0n"
+              'blue' : "\033[40;34m *********** ${value} *********** \033[0m\n",
+              'green' : "\033[40;32m *********** ${value} *********** \033[0m\n"
               ]
     ansiColor('xterm') {
         println(colors[color])
